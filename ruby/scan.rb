94bd60a3d273ec2604d9bd4af9c55045ba875ec3
Prasad <89862991+Laxdip@users.noreply.github.com>
2026-04-30 06:57:43 +0530

    Update scan.rb

--- ruby/scan.rb
+++ ruby/scan.rb
@@ -49,17 +49,6 @@ BANNER_PROBES = {
   27017 => "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
 }
 
-# Common ports with default services
-COMMON_PORTS = {
-  20 => "FTP-data", 21 => "FTP", 22 => "SSH", 23 => "Telnet",
-  25 => "SMTP", 53 => "DNS", 80 => "HTTP", 110 => "POP3",
-  111 => "RPC", 135 => "RPC", 139 => "NetBIOS", 143 => "IMAP",
-  443 => "HTTPS", 445 => "SMB", 993 => "IMAPS", 995 => "POP3S",
-  1723 => "PPTP", 3306 => "MySQL", 3389 => "RDP", 5432 => "PostgreSQL",
-  5900 => "VNC", 6379 => "Redis", 8080 => "HTTP-Alt", 8443 => "HTTPS-Alt",
-  27017 => "MongoDB"
-}
-
 def get_service_name(port)
   COMMON_PORTS[port] || "Unknown"
 end
