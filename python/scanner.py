35ed54849ade55b36ef13836030ae3dc548e9158
Prasad Bobde <89862991+Laxdip@users.noreply.github.com>
2026-05-20 08:30:03 +0530

    Remove color class and related banner printing

--- python/scanner.py
+++ python/scanner.py
@@ -1,16 +1,4 @@
 
-# Colors for terminal output
-class Colors:
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    BLUE = '\033[94m'
-    MAGENTA = '\033[95m'
-    CYAN = '\033[96m'
-    WHITE = '\033[97m'
-    RESET = '\033[0m'
-    BOLD = '\033[1m'
-
 def print_banner():
     banner = f"""
 {Colors.CYAN}{Colors.BOLD}
